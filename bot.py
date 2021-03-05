5ab2187c0238236a020689d6f017c7604b98819f
Tsujinaga Taisuke <74693813+johonescal@users.noreply.github.com>
2021-03-06 00:03:12 +0900

    bot.py upload

---
+++ bot.py
@@ -0,0 +1,15 @@
+import discord
+import analysis
+import os
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    anlys: int = analysis.main(message.content)
+
+    if anlys == 0:
+        await message.delete()
+        await client.send_message(message.channel, "モラルが守れていないメッセージを削除しました。")
+
+client.run(os.environ.get("discord_token"))
