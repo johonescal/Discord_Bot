5ae074bb3470c6c3b5ae3ed1afa52a873f54dd96
Tsujinaga Taisuke <74693813+johonescal@users.noreply.github.com>
2021-03-06 00:34:25 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,18 @@
 import discord
-import analysis
 import os
 
 client = discord.Client()
 
 @client.event
 async def on_message(message):
-    analys: int = 0
+    if message.author == client.user:
+        return
+
+    anlys: int = 0
     #anlys: int = analysis.main(message.content)
 
     if anlys == 0:
         await message.delete()
-        await client.send_message(message.channel, "モラルが守れていないメッセージを削除しました。")
+        await message.channel.send(message.author.mention + 'モラルが守れていないメッセージを削除しました。')
 
-client.run(os.environ.get("discord_token"))
+client.run(os.environ.get('discord_token'))
